3afc341554a11c7581fa45dc0c6472278fae9d3c
BananaSpritee <164941682+BananaSpritee@users.noreply.github.com>
2025-05-18 14:14:53 -0300

    .

--- functions/inserir.php
+++ functions/inserir.php
@@ -1,13 +1,23 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['nome'], $_POST['quantidade'], $_POST['unidade'], $_POST['valor'], $_POST['categoria_id'])) {
+
+    $sql = ("INSERT INTO produto (nome, quantidade, unidade, valor, categoria_id) VALUES (:nome, :quantidade, :unidade, :valor, :categoria_id)");
+    $stmt = $conn->prepare($sql);
+
     $nome = $_POST["nome"];
     $quantidade = $_POST["quantidade"];
     $unidade = $_POST["unidade"];
     $valor = $_POST["valor"];
     $categoria_id = $_POST["categoria_id"];
 
-    $sql = "INSERT INTO produto (nome, quantidade, unidade, valor, categoria_id) VALUES (?, ?, ?, ?, ?)";
-    $stmt = $conn->prepare($sql);
-    $stmt->execute([$nome, $quantidade, $unidade, $valor, $categoria_id]);
+
+    $stmt->bindParam(':nome', $nome);
+    $stmt->bindParam(':quantidade', $quantidade);
+    $stmt->bindParam(':unidade', $unidade);
+    $stmt->bindParam(':valor', $valor);
+    $stmt->bindParam(':categoria_id', $categoria_id);
+
+    $stmt->execute();
+
 }
 ?>
